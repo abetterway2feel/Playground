a4a2641d673d63e0219f5896d7b527900e1a6770
Phil Quinn <abetterway2feel@hotmail.co.uk>
2015-03-17 00:56:17 +0100

    updated ignorefile and refactored reverser

--- katas/src/stringreverse/IndividualStringRecursiveReverser.java
+++ katas/src/stringreverse/IndividualStringRecursiveReverser.java
@@ -1,38 +1,45 @@
 package stringreverse;
 
-public class IndividualStringRecursiveReverser{
+import java.text.StringCharacterIterator;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
-    private static String reverseStringPreserveChar(String forward, char separator) {
+public class IndividualStringRecursiveReverser {
+    private static LinkedList<Character> separators;
+
+    static {
+        separators = new LinkedList<Character>();
+        separators.add('"');
+        separators.add(' ');
+    }
+
+    private static String reverseStringPreserveChar(String forward, List<Character> separators) {
         String reverse = "";
         String currentWord = "";
-        for(char ch:forward.toCharArray()){
-            if(ch == separator){
-                if(currentWord.length() > 0){
+        StringCharacterIterator iterator = new StringCharacterIterator(forward);
+        char nextChar = iterator.current();
+        do{
+            if (separators.contains(nextChar) || iterator.getIndex() == iterator.getEndIndex()) {
+                if (currentWord.length() > 0) {
                     reverse = reverse + reverse(currentWord);
                     currentWord = "";
                 }
-                reverse = reverse + ch;
+                reverse = reverse + nextChar;
+            } else {
+                currentWord = currentWord + nextChar;
             }
-            else {
-                currentWord = currentWord + ch;
-            }
-        }
-        if(currentWord.length() > 0){
-            reverse = reverse + reverse(currentWord);
-        }
+        }while ( (nextChar = iterator.next()) != StringCharacterIterator.DONE);
+
         return reverse;
     }
 
     public static String reverse(String forward) {
-        if (forward.contains("\"")){
-            return reverseStringPreserveChar(forward, '"');
+        if (forward.contains("\"")) {
+            return reverseStringPreserveChar(forward, separators);
         }
 
-        if (forward.contains(" ")){
-            return reverseStringPreserveChar(forward, ' ');
-        }
-
-        if(forward.length() <= 1){
+        if (forward.length() <= 1) {
             return forward;
         }
         return reverse(forward.substring(1)) + forward.charAt(0);
